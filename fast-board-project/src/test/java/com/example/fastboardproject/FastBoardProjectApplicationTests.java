20246c912356db78428b09acbffdad9cf634d015
cosmos-csh <cosmos980313@naver.com>
2023-08-28 18:26:14 +0900

    #2 - 스프링 프로젝트 시작
    스프링 부트 초기화
    * 웹
    * 데브툴
    * 엑튜에이터
    * 롬복

---
+++ fast-board-project/src/test/java/com/example/fastboardproject/FastBoardProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fastboardproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastBoardProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
